929ca6c51f31a6ab95104ef3392d20b129d8f48e
Tanish719 <76243922+Tanish719@users.noreply.github.com>
2021-11-08 12:01:28 +0100

    Extras

--- lib/main.dart
+++ lib/main.dart
@@ -31,7 +31,7 @@ class MyApp extends StatelessWidget {
             // or simply save your changes to "hot reload" in a Flutter IDE).
             // Notice that the counter didn't reset back to zero; the application
             // is not restarted.
-            primarySwatch: Colors.orange,
+            primarySwatch: Colors.blueGrey,
           ),
           home: const MyHomePage(title: 'Flutter Demo Home Page'),
         ),
@@ -69,7 +69,7 @@ class _MyHomePageState extends State<MyHomePage> {
           );
         },
         child: const Icon(Icons.add),
-        backgroundColor: Colors.orange,
+        backgroundColor: Color(0xFF1D3557),
       ),
       body: SafeArea(
           child: SfCalendar(
@@ -139,58 +139,5 @@ class _MyHomePageState extends State<MyHomePage> {
   }
 }
 
-List<Meeting> _getDataSource() {
-  final List<Meeting> meetings = <Meeting>[];
-  final DateTime today = DateTime.now();
-  final DateTime startTime =
-      DateTime(today.year, today.month, today.day, 9, 0, 0);
-  final DateTime endTime = startTime.add(const Duration(hours: 2));
-  meetings.add(Meeting(
-      'Conference', startTime, endTime, const Color(0xFF0F8644), false));
-  meetings.add(Meeting(
-      'Kill Aditi', startTime, endTime, const Color(0xFFC51162), false));
-  meetings.add(Meeting('Doctor', DateTime(2021, 12, 18, 10),
-      DateTime(2021, 12, 18, 12), Colors.purple, false));
-  return meetings;
-}
 
-class MeetingDataSource extends CalendarDataSource {
-  MeetingDataSource(List<Meeting> source) {
-    appointments = source;
-  }
 
-  @override
-  DateTime getStartTime(int index) {
-    return appointments![index].from;
-  }
-
-  @override
-  DateTime getEndTime(int index) {
-    return appointments![index].to;
-  }
-
-  @override
-  String getSubject(int index) {
-    return appointments![index].eventName;
-  }
-
-  @override
-  Color getColor(int index) {
-    return appointments![index].background;
-  }
-
-  @override
-  bool isAllDay(int index) {
-    return appointments![index].isAllDay;
-  }
-}
-
-class Meeting {
-  Meeting(this.eventName, this.from, this.to, this.background, this.isAllDay);
-
-  String eventName;
-  DateTime from;
-  DateTime to;
-  Color background;
-  bool isAllDay;
-}
